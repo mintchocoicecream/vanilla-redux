4ad8d589bb3677195d38ce0cb0246fbbe8f6c626
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-02 15:58:07 +0900

    #3 React Redux Connecting the Store

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "../routes/Detail";
 import Home from "../routes/Home";
 
 function App() {
     return(
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/:id" exact element={<Detail />}></Route>
                 <Route path="/" exact element={<Home />}></Route>
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 }
 
